4378e3b04e1356a0fa8f7e058064d6f9406af254
asahara <asahara.chihiro.23e@jp.nssol.nipponsteel.com>
2022-02-09 11:35:05 +0900

    fix:reservation.tsのid追加

--- types/Reservation.ts
+++ types/Reservation.ts
@@ -1,6 +1,9 @@
 export type Reservation = {
+    theaterId?: number,
     theaterName?: string,
+    filmId?: number,
     filmName?: string,
+    scheduleId?: number,
     schedule?: string,
     count?: string,
     ticketType?: string,
