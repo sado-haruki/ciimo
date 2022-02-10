873e3415192378705a36c8c2b1830f3cc3275424
asahara <asahara.chihiro.23e@jp.nssol.nipponsteel.com>
2022-02-10 15:51:26 +0900

    feat:CSS調整

--- components/SeatButton.tsx
+++ components/SeatButton.tsx
@@ -1,33 +1,37 @@
-import React, { useState } from 'react'
-import styles from '../styles/components/SeatButton.module.scss';
-import { ReservationSeat } from '../types/Reservation';
+import React, { useState } from "react";
+import styles from "../styles/components/seatButton.module.scss";
+import { ReservationSeat } from "../types/Reservation";
 
 interface seatButtonProps {
-    key: number;
-    selectedSeat: ReservationSeat;
-    seat: ReservationSeat;
-    reserved: boolean;
-    clickSeat: Function;
+  key: number;
+  selectedSeat: ReservationSeat;
+  seat: ReservationSeat;
+  reserved: boolean;
+  clickSeat: Function;
 }
 
-const SeatButton = ({ key, selectedSeat, reserved, clickSeat, seat }: seatButtonProps) => {
-    const [selectedFlg, setSelectedFlg] = useState(false)
-    
-    return (
-        <button
-            key={key}
-            onClick={() => {
-                clickSeat(seat);
-                setSelectedFlg(!(selectedSeat === seat))
-            }}
-            className={
-                `${selectedFlg ? styles.selected : styles.notSelected} 
-                ${reserved ? styles.notClickablebutton : styles.clickablebutton}`
-            }
-        >
-            {seat.seatName}
-        </button>
-    )
-}
+const SeatButton = ({
+  key,
+  selectedSeat,
+  reserved,
+  clickSeat,
+  seat,
+}: seatButtonProps) => {
+  const [selectedFlg, setSelectedFlg] = useState(false);
+
+  return (
+    <button
+      key={key}
+      onClick={() => {
+        clickSeat(seat);
+        setSelectedFlg(!(selectedSeat === seat));
+      }}
+      className={`${selectedFlg ? styles.selected : styles.notSelected} 
+                ${reserved ? styles.disabled : styles.abled}`}
+    >
+      <span className={styles.number}>{seat.seatName}</span>
+    </button>
+  );
+};
 
 export default SeatButton;
--- pages/seatSelect/index.tsx
+++ pages/seatSelect/index.tsx
@@ -1,78 +1,89 @@
-import type { NextPage } from 'next'
-import React, { useState, useEffect, useRef, MouseEventHandler } from 'react'
-import axios from 'axios'
-import { Theater, Seat, Column } from '../../types/Theater'
-import { Reservation, ReservationSeat } from '../../types/Reservation';
-import Link from 'next/link';
-import Header from '../../components/Header';
+import type { NextPage } from "next";
+import React, { useState, useEffect, useRef, MouseEventHandler } from "react";
+import axios from "axios";
+import { Theater, Seat, Column } from "../../types/Theater";
+import { Reservation, ReservationSeat } from "../../types/Reservation";
+import Link from "next/link";
+import Header from "../../components/Header";
 
-import styles from '../../styles/seatSelect/index.module.scss'
-import SeatButton from '../../components/SeatButton';
+import styles from "../../styles/seatSelect/index.module.scss";
+import SeatButton from "../../components/SeatButton";
 
 const toJson = (data: string | null) => {
   return data ? JSON.parse(data) : null;
-}
+};
 
 const SeatSelect: NextPage = () => {
   const seats = useRef<Seat[]>([]);
   const reservationSeat = useRef<ReservationSeat>();
-  const [readFlg, setReadFlg] = useState(false)
+  const [readFlg, setReadFlg] = useState(false);
 
   useEffect(() => {
     theaterGetJson();
-  }, [])
+  }, []);
 
   const theaterGetJson = () => {
-    const reservation = toJson(localStorage.getItem('reservation')) as Reservation
-    axios.get("http://localhost:5000/theater/").then(response => {
-      const theaters: Theater[] = response.data
-      seats.current = theaters.find((theater) => theater.id === reservation.theaterId)?.film
-        .find((f) => f.id === reservation.filmId)?.schedule
-        .find((s) => s.id === reservation.scheduleId)?.seat || []
+    const reservation = toJson(
+      localStorage.getItem("reservation")
+    ) as Reservation;
+    axios.get("http://localhost:5000/theater/").then((response) => {
+      const theaters: Theater[] = response.data;
+      seats.current =
+        theaters
+          .find((theater) => theater.id === reservation.theaterId)
+          ?.film.find((f) => f.id === reservation.filmId)
+          ?.schedule.find((s) => s.id === reservation.scheduleId)?.seat || [];
       console.log(seats);
       setReadFlg(true);
-    })
-  }
+    });
+  };
 
   const clickSeat = (seat: ReservationSeat) => {
     const reservationSeatTemp: ReservationSeat = {
       row: seat.row,
       seatName: seat.seatName,
-    }
+    };
 
     reservationSeat.current = reservationSeatTemp;
-  }
+  };
 
   const clickConfirm = (e: any) => {
     e.stopPropagation();
     theaterGetJson();
 
-    const reservedTemp = seats.current.find((seat) => (seat.row === reservationSeat.current?.row))?.column
-      .find((columnTemp) => columnTemp.seatName === reservationSeat.current?.seatName)?.reserved
+    const reservedTemp = seats.current
+      .find((seat) => seat.row === reservationSeat.current?.row)
+      ?.column.find(
+        (columnTemp) =>
+          columnTemp.seatName === reservationSeat.current?.seatName
+      )?.reserved;
 
     if (reservedTemp) {
       e.preventDefault();
       return;
     }
 
-    localStorage.setItem('reservationSeat', JSON.stringify(reservationSeat.current));
-  }
+    localStorage.setItem(
+      "reservationSeat",
+      JSON.stringify(reservationSeat.current)
+    );
+  };
 
   return (
     <>
       <Header />
       <div className={styles.main}>
         <div className={styles.contants}>
-          {
-            // シート情報が取得できた場合
-            (readFlg) ? (
-              <>
-                <div className={styles.screen}>SCREEN</div>
-                {seats.current.map((seat, i) => (
-                  <div className={styles.seats} key={i}>
-                    <span className={styles.row}>{seat.row}</span>
-                    {
-                      seat.column.map((columTemp, j) => (
+          <div className={styles.selectField}>
+            {
+              // シート情報が取得できた場合
+              readFlg ? (
+                <>
+                  <div className={styles.screen}>SCREEN</div>
+                  {seats.current.map((seat, i) => (
+                    <div className={styles.seats} key={i}>
+                      <span className={styles.row}>{seat.row}</span>
+                      {seat.column.map((columTemp, j) => (
                         <SeatButton
                           key={j}
                           selectedSeat={reservationSeat.current || {}}
@@ -80,35 +91,36 @@ const SeatSelect: NextPage = () => {
                           clickSeat={clickSeat}
                           seat={{ row: seat.row, seatName: columTemp.seatName }}
                         />
-                      ))
-                    }
-                    <div key={i}></div>
-                  </div>
-                ))}
-              </>
-            ) :
-              // シート情報が取得できていない場合
-              (
+                      ))}
+                      <div key={i} className={styles.row}>
+                        {seat.row}
+                      </div>
+                    </div>
+                  ))}
+                </>
+              ) : (
+                // シート情報が取得できていない場合
                 <>読み込み中</>
               )
-          }
-          <span className={styles.returnAndConfirm}>
+            }
+          </div>
+          <div className={styles.buttons}>
             <Link href={"/schedule"}>
               <a>
-                <button className={styles.returnButton}>検索結果に戻る</button>
+                <button className={styles.back}>検索結果に戻る</button>
               </a>
             </Link>
 
             <Link href={"/confirm"}>
               <a onClick={clickConfirm}>
-                <button className={styles.confirmButton}>決定する</button>
+                <button className={styles.confirm}>決定する</button>
               </a>
             </Link>
-          </span>
+          </div>
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default SeatSelect
+export default SeatSelect;
