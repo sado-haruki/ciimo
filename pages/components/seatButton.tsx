363faa7cddfdf75e8f7ac212f265aa69256c2d37
Ueno <ueno.daichi.x4d@jp.nssol.nipponsteel.com>
2022-02-10 14:11:34 +0900

    cssの調整

---
+++ pages/components/seatButton.tsx
@@ -0,0 +1,33 @@
+import React, { useState, useEffect, useRef, MouseEventHandler } from 'react'
+import styles from '../../styles/components/seatButton.module.scss';
+import { ReservationSeat } from '../../types/Reservation';
+
+interface seatButtonProps {
+    key: number;
+    selectedSeat: ReservationSeat;
+    seat: ReservationSeat;
+    reserved: boolean;
+    clickSeat: Function;
+}
+
+const SeatButton = ({ key, selectedSeat, reserved, clickSeat, seat }: seatButtonProps) => {
+    const [selectedFlg, setSelectedFlg] = useState(false)
+    
+    return (
+        <button
+            key={key}
+            onClick={() => {
+                clickSeat(seat);
+                setSelectedFlg(!(selectedSeat === seat))
+            }}
+            className={
+                `${selectedFlg ? styles.selected : styles.notSelected} 
+                ${reserved ? styles.notClickablebutton : styles.clickablebutton}`
+            }
+        >
+            {seat.seatName}
+        </button>
+    )
+}
+
+export default SeatButton;
